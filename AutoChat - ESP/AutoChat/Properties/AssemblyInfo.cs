24a2f456acc82195da0e62d2558819a969a3b154
stigma <navillychan@gmail.com>
2016-02-07 23:00:49 -0300

    lc4

--- AutoChat - ESP/AutoChat/Properties/AssemblyInfo.cs
+++ AutoChat - ESP/AutoChat/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoChat")]
+[assembly: AssemblyTitle("AutoChat - ESP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoChat")]
+[assembly: AssemblyProduct("AutoChat - ESP")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
